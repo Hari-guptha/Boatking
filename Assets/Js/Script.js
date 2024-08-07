8e0a8362638dce14241d09443a8aa1fd642435e2
Hari-guptha <harihariguptha@gmail.com>
2024-08-08 03:51:52 +0530

    v1

---
+++ Assets/Js/Script.js
@@ -0,0 +1,11 @@
+const tiggerpopup = () => {
+    const pop = document.getElementById('NavPopUp');
+    pop.style.display = "block";
+
+}
+
+
+const ClosePopUp = () => {
+    const pop = document.getElementById('NavPopUp');
+    pop.style.display = "none";
+}
